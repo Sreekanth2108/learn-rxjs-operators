64bbaa0b28b50cb91fc92c348a45ad23dcd0b482
Sreekanth2108 <sreekanth2108@gmail.com>
2020-06-28 09:05:07 -0700

    Added reference to learn rxjs chrome extension

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@
  * Copy code from the <rxjs-operator>.ts file and paste it here to play with that operator and see the results in the console
  *********/ 
 
+/**********
+ * Do checkout this chrome extension to learn an operator a day https://chrome.google.com/webstore/detail/learn-rxjs/kegaojpfbgbhmpigpejnobophefcmmom
+ */
 
 
 
